5c4a5aeebbc9c4a2ae8f6f0446c610a43ef3827c
Artax601 <artaxw1@gmail.com>
2023-12-12 19:44:34 +0100

    Updated carousel arrow script and added pagination dots

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,45 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+// let boutons = document.getElementsByClassName("arrow_left").addEventListener("click");
+
+// let btn = document.getElementById("btn-left").addEventListener("click");
+
+// console.log(btn);
+
+// document.getElementById("arrow_left").addEventListener("click", function () {
+//   alert("Hello World!");
+// });
+
+// document.getElementById("arrow_right").addEventListener("click", function () {
+//   console.log("HAHAH");
+// });
+
+document.getElementById("arrow_left").addEventListener("click", myFunction);
+
+function myFunction() {
+  console.log("Gauche");
+}
+
+document.getElementById("arrow_right").addEventListener("click", btnRight);
+
+function myFunction() {
+  console.log("Droite");
+}
